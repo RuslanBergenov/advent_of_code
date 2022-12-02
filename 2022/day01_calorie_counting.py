47f5439a868262ae067fae3d2d1e44defbe2c701
RuslanBergenov <bergenov@ualberta.ca>
2022-12-01 19:43:30 -0700

    feat: day 1 - calorie counting - read in all lines in file except for the last one

---
+++ 2022/day01_calorie_counting.py
@@ -0,0 +1,58 @@
+"""
+https://adventofcode.com/2022/day/1
+https://adventofcode.com/2022/day/1/input
+
+read input into a list of lists
+reduce each list to its today
+get the max total
+"""
+
+import unittest
+import os
+
+class Solution(object):
+    #TODO: refactor so it's several (2?) smaller functions
+    def count_calories(self, file_name):
+        """
+
+        :param file_name:
+        :return:
+        """
+        elves_and_calories = []
+
+        file_path = os.path.join(
+            os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '2022'), 'resources'),
+            file_name)
+
+        file_path = os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), file_name))
+
+        file = open(file_path, "r")
+
+        # https://stackoverflow.com/questions/3277503/how-to-read-a-file-line-by-line-into-a-list
+        elf_calories = []
+        for calorie_amount in file:
+
+            if calorie_amount == "\n":
+                elves_and_calories.append(elf_calories)
+                elf_calories = []
+            else:
+                elf_calories.append(calorie_amount)
+                # elves_and_calories.append(int(calorie_amount.replace("/n", "")))
+
+        # The "for-else" design
+        # https://stackoverflow.com/questions/10140281/how-to-find-out-whether-a-file-is-at-its-eof
+        # https://docs.python.org/2/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
+        # else:
+        #     elves_and_calories.append(elf_calories)
+        file.close()
+
+        max_calories = 0
+        for elfs_calories in elves_and_calories:
+            max_calories = max(max_calories, sum(elfs_calories))
+        return max_calories
+
+
+
+class TestCase(unittest.TestCase):
+    def test_function_0(self):
+        assert Solution().count_calories("2022/resources/day01_calorie_counting_test.txt") == 24000
