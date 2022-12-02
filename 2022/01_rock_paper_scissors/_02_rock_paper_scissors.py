36e33ae7166007d78f5de82723e89879e487178c
RuslanBergenov <bergenov@ualberta.ca>
2022-12-02 12:13:19 -0700

    feat: day 2 work in progress - answer is too low

---
+++ 2022/01_rock_paper_scissors/_02_rock_paper_scissors.py
@@ -0,0 +1,72 @@
+# https://adventofcode.com/2022/day/2
+
+"""
+A 1st player rock
+B 1st player paper
+C 1st player scissors
+
+X 2nd player rock
+Y 2nd player paper
+Z 2nd player scissors
+
+
+rock        1
+paper       2
+scissors    3
+
+victory     6
+
+winning combinations for 2nd player:
+A Y
+B Z
+C X
+"""
+
+
+import unittest
+
+class Solution(object):
+    def rock_paper_scissors(self, file_name):
+        moves_mapping = {"A": "X",
+                         "B": "Y",
+                         "C": "Z"}
+
+        winning_combinations = {"A Y",
+                                "B Z",
+                                "C X"}
+        participation_scores = {"X": 1,
+                                "Y": 2,
+                                "Z": 3}
+        score = 0
+        with open(file_name) as f:
+            for line in f:
+                print(line)
+
+                line_cleaned = line.replace("\n","")
+                if line_cleaned in winning_combinations:
+                    score += 6
+                my_move = line_cleaned.split()[1]
+                oppenents_move_translated = moves_mapping[line_cleaned.split()[0]]
+
+                score += participation_scores[my_move]
+
+                if my_move == oppenents_move_translated:
+                    score += participation_scores[my_move]
+
+        print(score)
+        return score
+
+
+
+
+
+
+
+class TestCase(unittest.TestCase):
+
+    def test_dev(self):
+        assert Solution().rock_paper_scissors("02_input_test.txt") == 15
+
+    def test_prod(self):
+        assert Solution().rock_paper_scissors("02_input.txt") == 12992
+
