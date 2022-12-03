b42381361a56ee69d0edb374bb6b0c068a5acfa0
RuslanBergenov <bergenov@ualberta.ca>
2022-12-03 12:51:59 -0700

    feat: day 3 part 1

---
+++ 2022/03_rucksack_reorganization/_03_01_rucksack_reorganization.py
@@ -0,0 +1,70 @@
+"""
+https://adventofcode.com/2022/day/3
+Day 3
+Read in line
+Split line into town parts
+Create two sets
+Set intersect
+ASCII
+Convert to score(s)
+Increment running total
+
+
+If Ord(X)
+
+65-90?
+65-38=27
+
+97-122?
+97-96=1
+
+What if length of input is odd number???
+"""
+
+"""
+Docs
+https://www.geeksforgeeks.org/python-split-string-into-list-of-characters/
+
+https://medium.com/interview-buddy/handling-ascii-character-in-python-58993859c38e
+"""
+
+import unittest
+
+
+class Solution(object):
+    def rucksack(self, file_name):
+        result = 0
+        with open(file_name) as f:
+            for line in f:
+                length = len(line)
+
+                rucksack_left = line[0:length//2]
+                rucksack_right = line[length//2:length]
+                len_rucksack_left = len(rucksack_left)
+                len_rucksack_right = len(rucksack_right)
+                assert len_rucksack_left + len_rucksack_right == length or len_rucksack_left + len_rucksack_right + 1 == length
+                list_left=[*rucksack_left]
+                list_right=[*rucksack_right]
+                set_left = set(list_left)
+                set_right = set(list_right)
+
+                common_items = set_left.intersection(set_right)
+
+                for item in common_items:
+                    ascii_code = ord(item)
+
+                    if 97 <= ascii_code <= 122:
+                        result += (ascii_code-96)
+                    elif 65 <= ascii_code <= 90:
+                        result += (ascii_code - 38)
+        print(f"result: {result}")
+        return result
+
+
+class TestCase(unittest.TestCase):
+
+    def test_dev(self):
+        assert Solution().rucksack("03_input_test.txt") == 157
+
+    def test_prod(self):
+        assert Solution().rucksack("03_input.txt") == 8072
