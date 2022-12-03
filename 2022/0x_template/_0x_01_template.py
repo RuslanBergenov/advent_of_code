fbe5b93517cf1a8d9a1c49b421a09493aaf2ab8c
RuslanBergenov <bergenov@ualberta.ca>
2022-12-03 13:20:36 -0700

    refactor: template

--- 2022/0x_template/_0x_01_template.py
+++ 2022/0x_template/_0x_01_template.py
@@ -22,5 +22,5 @@ class TestCase(unittest.TestCase):
     def test_dev(self):
         assert Solution().fuction("0x_input_test.txt") == 0
 
-    def test_dev(self):
+    def test_prod(self):
         assert Solution().fuction("0x_input.txt") == 1
