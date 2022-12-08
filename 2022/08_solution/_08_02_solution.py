3809f19548f534ea3f63210c3bf0b829cd7d8c29
RuslanBergenov <bergenov@ualberta.ca>
2022-12-08 10:08:11 -0700

    feat: day 8 part 2 - started

---
+++ 2022/08_solution/_08_02_solution.py
@@ -0,0 +1,50 @@
+"""
+Plan:
+"""
+
+"""
+Docs:
+
+"""
+
+import unittest
+
+
+
+def solution(file_name):
+
+    forest = []
+    with open(file_name) as f:
+        for row in f:
+            forest.append(list(row.replace("\n", "")))
+    result = 0
+    for irow, vrow in enumerate(forest):
+        for itree, vtree in enumerate(forest[irow]):
+
+                nb_left = forest[irow][0:itree]
+                nb_right = forest[irow][itree+1:]
+
+                nb_above = []
+                for irow_above in range(0, irow):
+                    nb_above.append(forest[irow_above][itree])
+                nb_below = []
+                for ir_below in range(irow + 1, len(forest)):
+                    nb_below.append(forest[ir_below][itree])
+
+                try:
+                    if vtree > max(nb_left) or vtree > max(nb_right) or vtree > max(nb_above) or vtree > max(nb_below):
+                        result += 1
+                except Exception as e:
+                    print(e)
+
+
+    return result
+
+
+class TestCase(unittest.TestCase):
+
+    def test_dev(self):
+        self.assertEqual(8, solution("08_input_test.txt"))
+
+    def test_prod(self):
+        self.assertEqual(1787, solution("08_input.txt"))
