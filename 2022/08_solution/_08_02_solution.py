edca68a6e7d21cd1f4677035dacaf19901165823
RuslanBergenov <bergenov@ualberta.ca>
2022-12-08 10:30:57 -0700

    feat: day 8 part 2 - solved

--- 2022/08_solution/_08_02_solution.py
+++ 2022/08_solution/_08_02_solution.py
@@ -4,7 +4,7 @@ Plan:
 
 """
 Docs:
-
+https://stackoverflow.com/questions/529424/traverse-a-list-in-reverse-order-in-python
 """
 
 import unittest
@@ -21,21 +21,51 @@ def solution(file_name):
     for irow, vrow in enumerate(forest):
         for itree, vtree in enumerate(forest[irow]):
 
-                nb_left = forest[irow][0:itree]
-                nb_right = forest[irow][itree+1:]
+            nb_left = forest[irow][0:itree]
+            nb_right = forest[irow][itree+1:]
 
-                nb_above = []
-                for irow_above in range(0, irow):
-                    nb_above.append(forest[irow_above][itree])
-                nb_below = []
-                for ir_below in range(irow + 1, len(forest)):
-                    nb_below.append(forest[ir_below][itree])
+            nb_above = []
+            for irow_above in range(0, irow):
+                nb_above.append(forest[irow_above][itree])
+            nb_below = []
+            for ir_below in range(irow + 1, len(forest)):
+                nb_below.append(forest[ir_below][itree])
 
-                try:
-                    if vtree > max(nb_left) or vtree > max(nb_right) or vtree > max(nb_above) or vtree > max(nb_below):
-                        result += 1
-                except Exception as e:
-                    print(e)
+            # current_tree_score = 0
+            #  left and above - reverse
+            score_left = 0
+            for index_neighbor, value_neighbor in enumerate(nb_left[::-1]):
+                if vtree > value_neighbor:
+                    score_left += 1
+                elif vtree <= value_neighbor:
+                    score_left += 1
+                    break
+
+            score_above = 0
+            for index_neighbor, value_neighbor in enumerate(nb_above[::-1]):
+                if vtree > value_neighbor:
+                    score_above += 1
+                elif vtree <= value_neighbor:
+                    score_above += 1
+                    break
+
+            score_right = 0
+            for index_neighbor, value_neighbor in enumerate(nb_right):
+                if vtree > value_neighbor:
+                    score_right += 1
+                elif vtree <= value_neighbor:
+                    score_right += 1
+                    break
+
+            score_below = 0
+            for index_neighbor, value_neighbor in enumerate(nb_below):
+                if vtree > value_neighbor:
+                    score_below += 1
+                elif vtree <= value_neighbor:
+                    score_below += 1
+                    break
+            current_tree_score = score_left * score_above * score_right * score_below
+            result = max(result, current_tree_score)
 
 
     return result
@@ -47,4 +77,4 @@ class TestCase(unittest.TestCase):
         self.assertEqual(8, solution("08_input_test.txt"))
 
     def test_prod(self):
-        self.assertEqual(1787, solution("08_input.txt"))
+        self.assertEqual(440640, solution("08_input.txt"))
