a75f3a6b5b8d06e58f70bf91dee8340e644cfc57
RuslanBergenov <bergenov@ualberta.ca>
2022-12-08 23:19:41 -0700

    feat: movement to the right similated

--- 2022/09_rope_bridge/_09_01_solution.py
+++ 2022/09_rope_bridge/_09_01_solution.py
@@ -11,49 +11,48 @@ Docs:
 """
 
 import unittest
-
+import copy
 
 class Solution(object):
+
+
     def fuction(self, file_name, board_size):
         board = []
-        indexes = []
+
         for i in range(board_size):
             row = []
-            visited_row = []
             for j in range(board_size):
-                row.append(j)
-                visited_row.append(0)
-            indexes.append(row)
-            board.append(visited_row)
+                row.append(0)
+            board.append(row)
+        board_tail_visited = copy.deepcopy(board)
+        board_original_state = copy.deepcopy(board)
 
         s_row = T_row = H_row = board_size - 1
         s_col = T_col = H_col = 0
 
-        board[s_row][s_col] = "H"
+        board[T_row][T_col] = "T"
+        board[H_row][H_col] = "H"
+        board_tail_visited[T_row][T_col] = 1
 
         with open(file_name) as f:
             for line in f:
                 direction = line.split()[0]
                 moves_number = int(line.split()[1])
 
-                board[H_row][H_col] = "H"
-                board[T_row][T_col] = "T"
+                for move in range(moves_number):
 
-                board[H_row][H_col] = 1
-
-                if direction == "R":
-                    for move in range(moves_number):
+                    if direction == "R":
 
                         H_col += 1
 
-                        if H_col - T_col >=1:
+                        if H_col - T_col > 1:
                             T_col+=1
 
-        result = 0
+                    board = copy.deepcopy(board_original_state)
 
-        return result
-
-
+                    board[H_row][H_col] = "H"
+                    board[T_row][T_col] = "T"
+                    board_tail_visited[T_row][T_col] = 1
 class TestCase(unittest.TestCase):
 
     def test_dev(self):
