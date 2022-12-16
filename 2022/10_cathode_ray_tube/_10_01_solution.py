83db2e79269f0dca7d88203ad7489d00d8dd8c2a
RuslanBergenov <bergenov@ualberta.ca>
2022-12-15 18:35:44 -0700

    refactor: day 9 - part 2 - struggling, can't solve this one

---
+++ 2022/10_cathode_ray_tube/_10_01_solution.py
@@ -0,0 +1,27 @@
+"""
+Plan:
+"""
+
+"""
+Docs:
+
+"""
+
+import unittest
+
+
+
+def fuction(self, file_name):
+    register = 1
+    interesting_cycles = [20, 60, 100, 140, 180, 220]
+
+    result = 0
+    return result
+
+
+
+def test_dev(self):
+    assert fuction("10_input_test.txt") == 13140
+
+# def test_prod(self):
+#     assert Solution().fuction("10_input.txt") == 1
